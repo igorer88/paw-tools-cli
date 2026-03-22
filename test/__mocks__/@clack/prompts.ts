27388f954e7f4b52825002e6637772c7cf051bd8
Igor Escalona <igorer88@users.noreply.github.com>
2026-03-22 17:15:35 -0400

    fix: move E2E mock to test/__mocks__ folder

---
+++ test/__mocks__/@clack/prompts.ts
@@ -0,0 +1,8 @@
+export const text = jest.fn()
+export const confirm = jest.fn()
+export const spinner = jest.fn(() => ({
+  start: jest.fn(),
+  stop: jest.fn()
+}))
+export const cancel = jest.fn()
+export const isCancel = jest.fn(() => false)
