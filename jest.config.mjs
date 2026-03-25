57ab0df69e0919bc17be1547fa3e0e6de6034f54
Igor Escalona <igorer88@users.noreply.github.com>
2026-03-25 16:27:43 -0400

    chore: add coverage threshold to prevent regression

--- jest.config.mjs
+++ jest.config.mjs
@@ -12,4 +12,12 @@ export default {
     '^@/(.*)$': '<rootDir>/$1',
     '^@clack/prompts$': '<rootDir>/../test/__mocks__/@clack/prompts.ts',
   },
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
